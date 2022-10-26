554f61fa339af606d0aee55f11e681e2a7622936
ch4rli3kop <pch2180@gmail.com>
2022-10-26 17:31:45 +0900

    Add simple evaluation function

--- CWE242_Use_of_Inherently_Dangerous_Function/basic_01-18.py
+++ CWE242_Use_of_Inherently_Dangerous_Function/basic_01-18.py
@@ -1,38 +1,38 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from binaryninja import *
+from utils.evaluation import get_all_files_from_path, get_answer, evaluate_result
 
-bv = binaryview.BinaryViewType.get_view_of_file('/Users/ch4rli3kop/binary-nomaj/test_workspace/CWE242_Use_of_Inherently_Dangerous_Function__basic_01.out')
+def solution(bv: BinaryViewType) -> list:
 
-targets = {
-    'gets': [0],
-    '_gets': [0]
-}
+    result = [] # spicious function list
 
-result = [] # spicious function list
+    targets = {
+        'gets': [0],
+        '_gets': [0]
+    }
 
-for target in targets:
-    target = bv.get_functions_by_name(target)
-    if len(target) < 1:
-        continue
-    target = target[0]
-    xrefs = bv.get_code_refs(target.start)
-    for ref in xrefs:
-        text = f'suspicous point : {ref.address:#x} at function {ref.function.start:#x}'
-        print(text)
-        result.append(ref.function)
+    for target in targets:
+        target = bv.get_functions_by_name(target)
+        if len(target) < 1:
+            continue
+        target = target[0]
+        xrefs = bv.get_code_refs(target.start)
+        for ref in xrefs:
+            text = f'suspicous point : {ref.address:#x} at function {ref.function.start:#x}'
+            #print(text)
+            result.append(ref.function)
+    
+    return result
 
-# evaluate results
-# Todo: make listing bad function more clear (this case does not address *_badsink function)
-# Todo: make comparing result with the real bad function more clear
-import re
-bad_functions = [func for func in bv.functions if re.match('_CWE.*_bad', func.name)]
-for func in bad_functions:
-    if func in result:
-        result.pop()
-    else:
-        print(f'you cant detect the bad function at {func.start:#x}')
+if __name__ == '__main__':
+    binary_path = '/Users/ch4rli3kop/binary-nomaj/Juliet_1.3/testcases/CWE242_Use_of_Inherently_Dangerous_Function/'
+    files = get_all_files_from_path(binary_path)
 
-if len(result) > 0:
-    for func in result:
-        print(f'false positive at {func.start:#x}')
-else:
-    print(f'gooood')
+    for file in files:
+        bv = binaryview.BinaryViewType.get_view_of_file(file)
+        result = solution(bv)
+        answer = get_answer(bv)
+        evaluate_result(result, answer)
+        
