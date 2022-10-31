68953e7d9df05b3c12921359f678cb22bbec3068
ch4rli3kop <pch2180@gmail.com>
2022-10-31 17:45:56 +0900

    Update utiles.py
    style: use path.glob instead of os.walk
    
    add depth_level when listing files from path at get_all_files_from_path

--- utils/utils.py
+++ utils/utils.py
@@ -1,18 +1,22 @@
-import os
 import re
-
+from pathlib import Path
 from binaryninja.binaryview import BinaryViewType
 
 
-def get_all_files_from_path(path: str, file_type: str = '.out') -> list[str]:
-    file_list = []
-    for root, _, files in os.walk(path):
-        file_list = [os.path.join(root,file) for file in files if file.endswith(file_type)]
+def get_all_files_from_path(path: str, depth_level: int = None, file_type: str = '.out') -> list[Path]:
+
+    base_directory = Path(path)
+    pattern = '**/*' # this means visiting all subdirectories recursively
+
+    if depth_level is not None:
+        pattern = '/'.join('*' * depth_level)
+
+    file_list = [file for file in base_directory.glob(pattern) if file.is_file() and not file.name.startswith('.') and file.name.endswith(file_type)]
     return file_list
 
-def get_matched_files_from_path(path: str, reg: str = '.*', file_type: str = '.out') -> list[str]:
-    file_list = get_all_files_from_path(path, file_type)
-    n_file_list = [file for file in file_list if re.match(reg, file)]
+def get_matched_files_from_path(path: str, reg: str = '.*', depth_level: int = None, file_type: str = '.out') -> list[Path]:
+    file_list = get_all_files_from_path(path, depth_level, file_type)
+    n_file_list = [file for file in file_list if re.match(reg, file.name)]
     return n_file_list
 
 def is_cpp_binary(bv: BinaryViewType) -> bool:
