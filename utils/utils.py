fb915cc8ce93e0b06bc307807f332aa91763ca9b
ch4rli3kop <pch2180@gmail.com>
2022-10-31 15:20:15 +0900

    Add get_matched_files_from_path
    feat: Add get_matched_files_from_path
    
    Resolves: #2

--- utils/utils.py
+++ utils/utils.py
@@ -10,8 +10,10 @@ def get_all_files_from_path(path: str, file_type: str = '.out') -> list[str]:
         file_list = [os.path.join(root,file) for file in files if file.endswith(file_type)]
     return file_list
 
-def get_matched_files_from_path(path, file_type='.out'):
-    raise NotImplemented
+def get_matched_files_from_path(path: str, reg: str = '.*', file_type: str = '.out') -> list[str]:
+    file_list = get_all_files_from_path(path, file_type)
+    n_file_list = [file for file in file_list if re.match(reg, file)]
+    return n_file_list
 
 def is_cpp_binary(bv: BinaryViewType) -> bool:
     
