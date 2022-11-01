456fd4517aca2d0aafd92f2550fee2e970e2dcbc
ch4rli3kop <pch2180@gmail.com>
2022-11-01 14:51:21 +0900

    Update CWE190

--- CWE190_Integer_Overflow/char_add.py
+++ CWE190_Integer_Overflow/char_add.py
@@ -7,8 +7,9 @@ from binaryninja import *
 from binaryninja.binaryview import BinaryViewType
 from binaryninja.function import Function
 from utils.runner import Runner
-from utils.utils import get_all_files_from_path
+from utils.utils import get_all_files_from_path, get_matched_files_from_path
 
+from z3 import *
 
 def solution(bv: BinaryViewType) -> list[Function]:
 
@@ -21,32 +22,58 @@ def solution(bv: BinaryViewType) -> list[Function]:
                 if inst.src.operation == MediumLevelILOperation.MLIL_ADD:
 
                     # FIXME: 우항이 상수일 때 추가
+                    if type(inst.src.right) == MediumLevelILConst:
+                        # a = b + c(constant value)
+                        a = inst.dest
+                        b = inst.src.left.src
+                        c = inst.src.right.constant
 
-                    if type(inst.src.right) == binaryninja.mediumlevelil.MediumLevelILVarSsa:
+                        if a.var.type.get_string() == 'char':
+                            #pv_a = pv_a.signed_range_value([ValueRange(-128, 127, 1)])
+                            
+                            print('zz')
+
+
+
+                    if type(inst.src.right) == MediumLevelILVarSsa:
                         # a = b + c
                         a = inst.dest
                         b = inst.src.left.src
                         c = inst.src.right.src
                         #print(a, b, c)
-                        a_range = inst.get_ssa_var_possible_values(a)
-                        b_range = inst.get_ssa_var_possible_values(b)
-                        c_range = inst.get_ssa_var_possible_values(c)
+                        pv_a = inst.get_ssa_var_possible_values(a)
+                        pv_b = inst.get_ssa_var_possible_values(b)
+                        pv_c = inst.get_ssa_var_possible_values(c)
 
                         if a.var.type.get_string() == 'char':
-                            a_range = a_range.signed_range_value([ValueRange(-128, 127, 1)])
-                            # print(f'{func.start:#x}', inst, a_range, b_range, c_range)
-                            try:
-                                # FIXME: sometimes it can be RegisterValueType.InSetOfValues type
-                                if a_range.ranges[0].end < b_range.ranges[0].end + c_range.ranges[0].end:
+                            pv_a = pv_a.signed_range_value([ValueRange(-128, 127, 1)])
+                            # print(f'{func.start:#x}', inst, pv_a, pv_b, pv_c)
+                            
+                            if pv_b.type in {RegisterValueType.SignedRangeValue, RegisterValueType.UnsignedRangeValue} and \
+                                pv_c.type in {RegisterValueType.SignedRangeValue, RegisterValueType.UnsignedRangeValue}:
+                                if pv_a.ranges[0].end < pv_b.ranges[0].end + pv_c.ranges[0].end or \
+                                    pv_a.ranges[0].start > pv_b.ranges[0].start + pv_c.ranges[0].start: # integer underflow
                                     #print('integer overflow!')
                                     result.append(func)
-                            except:
-                                pass
+                            
+                            # TODO: InSetOfValue
+                            
+
+                            # Is there the case that X = InSetOfValues + RangeValue?
+                            elif pv_b.type == RegisterValueType.InSetOfValues and \
+                                pv_c.type in {RegisterValueType.SignedRangeValue, RegisterValueType.UnsignedRangeValue}:
+                                raise
+                                
+                            elif pv_b.type in {RegisterValueType.SignedRangeValue, RegisterValueType.UnsignedRangeValue} and \
+                                pv_c.type == RegisterValueType.InSetOfValues :
+                                raise
+
     return result
 
 if __name__ == '__main__':
-    binary_path = '/Users/ch4rli3kop/binary-nomaj/integer_overflow_workspace'
-    file_list = get_all_files_from_path(binary_path)
-
+    #binary_path = '/Users/ch4rli3kop/binary-nomaj/integer_overflow_workspace/'
+    binary_path = '/Users/ch4rli3kop/binary-nomaj/Juliet_amd64/testcases/CWE190_Integer_Overflow/'
+    #file_list = get_all_files_from_path(binary_path, 1)
+    file_list = get_matched_files_from_path(binary_path, ".*__char_.*_multiply.*out")
     runner = Runner(solution, file_list)
     runner.run()
