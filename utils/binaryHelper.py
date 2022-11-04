558b6a2b0abe9e31995d723784f6f29205128a10
ch4rli3kop <pch2180@gmail.com>
2022-11-04 15:56:00 +0900

    Update BinaryHelper

--- utils/binaryHelper.py
+++ utils/binaryHelper.py
@@ -14,25 +14,45 @@
     각 C++ 바이너리를 관리하는 클래스로, 하나의 객체당 하나의 바이너리와 관련된 모든 작업을 수행하는 역할.
     CBinaryHelper를 상속하며, C++ 바이너리의 경우 function name이 mangle 되어 있으므로, demangle 하는 과정을 추가로 수행
 
+    TODO: run 과정에서 answer 구하게하기
+    
+
 '''
 from pathlib import Path
 from binaryninja import *
 from utils.utils import is_cpp_binary
+import networkx as nx
 
-class CBinaryHelper:
 
-    def __init__(self, file: Path = None) -> None:
-        assert file is not None
-        
-        self.bv: BinaryView = BinaryViewType.get_view_of_file(file.absolute())
+class CBinaryHelper():
+
+    def __init__(self, bv: BinaryView = None) -> None:
+        assert bv is not None
+
+        self.bv = bv
         self.platform: str = self.bv.platform.name
-        self.is_cpp: bool = is_cpp_binary(self.bv)
+        self.bad_function = []
+        self.bad_function_path = nx.DiGraph()
 
     def run(self, solution = None):
         assert solution is not None
-        return solution(self.bv)
+        self.result = solution(self.bv)
+        self.bad_function = self._find_answer()
+        self.bad_function_path = self._find_answer_function_path()
 
-    def get_answer(self) -> list[Function]:
+    # @property
+    # def result(self):
+    #     return self.result
+
+    @property
+    def answer(self):
+        return self.bad_function
+    
+    @property
+    def answer_path(self):
+        return self.bad_function_path
+
+    def _find_answer(self) -> list[Function]:
         # FIXME: this code work correctly only when binary has one vulnerability
         bad_functions = [func for func in self.bv.functions if re.match('_?CWE.*badSink', func.name)]
 
@@ -50,35 +70,100 @@ class CBinaryHelper:
         
         return bad_functions
 
+    def _find_answer_function_path(self):
+        bad_function = [func for func in self.bv.functions if re.match('_?CWE.*bad$', func.name)]
+        if len(bad_function) < 1:
+            raise
+        bad_function = bad_function[0]
+        self.bad_function_path.add_node(bad_function.name)
+
+        # badSink
+        bad = bad_function
+        while True:
+            prev = bad.name
+            bad = [func for func in bad.callees if re.match('.*bad.*Sink', func.name)]
+            if len(bad) < 1:
+                break
+            bad = bad[0]
+            self.bad_function_path.add_node(bad.name)
+            self.bad_function_path.add_edge(prev, bad.name)
+
+        # badSource
+        bad = bad_function
+        while True:
+            prev = bad.name
+            bad = [func for func in bad.callees if re.match('.*bad.*Source', func.name)]
+            if len(bad) < 1:
+                break
+            bad = bad[0]
+            self.bad_function_path.add_node(bad.name)
+            self.bad_function_path.add_edge(prev, bad.name)
+
+        return self.bad_function_path
+        
+
 class CPPBinaryHelper(CBinaryHelper):
     def __init__(self, file: Path) -> None:
-        super().__init__(file=file)
+        super().__init__(file)
+        self.platform: str = self.bv.platform.name
+
+
+    def demangle_func_name(self, func: str) -> str:
+        if func[:2] == '_Z':
+            if self.platform.split('-')[0] == 'linux':
+                name = demangle_gnu3(self.bv.arch, func)[1]
+            elif self.platform.split('-')[0] == 'windows':
+                name = demangle_ms(self.bv.arch, func)[1]
+            func_name = get_qualified_name(name)
+            return func_name
+        else:
+            return func
 
     def get_answer(self) -> list[Function]:
         # FIXME: this code work correctly only when binary has one vulnerability
-        bad_functions = [func for func in self.bv.functions if re.match('_?CWE.*badSink', self.demangle_function(func))]
+        bad_functions = [func for func in self.bv.functions if re.match('_?CWE.*badSink', self.demangle_func_name(func.name))]
 
         # when call like 54b_badSink -> 54c_badSink -> 54d_badSink ... it return max badSink function
         if len(bad_functions) > 1:
-            bad_functions = { self.demangle_function(func) : func for func in self.bv.functions if re.match('_?CWE.*badSink', self.demangle_function(func))}
+            bad_functions = { self.demangle_func_name(func.name) : func for func in self.bv.functions if re.match('_?CWE.*badSink', self.demangle_func_name(func.name))}
             bad_functions = [ bad_functions[max(bad_functions.keys())] ]
 
         # some binary has just "badSink" function
         if len(bad_functions) < 1:
-            bad_functions = [func for func in self.bv.functions if re.match('badSink', self.demangle_function(func))]
+            bad_functions = [func for func in self.bv.functions if re.match('badSink', self.demangle_func_name(func.name))]
 
         if len(bad_functions) < 1:
-            bad_functions = [func for func in self.bv.functions if re.match('_?CWE.*bad$', self.demangle_function(func))]
+            bad_functions = [func for func in self.bv.functions if re.match('_?CWE.*bad$', self.demangle_func_name(func.name))]
         
         return bad_functions
 
-    def demangle_function(self, func: Function) -> str:
-        if func.name[:2] == '_Z':
-            if self.platform.split('-')[0] == 'linux':
-                name = demangle_gnu3(self.bv.arch, func.name)[1]
-            elif self.platform.split('-')[0] == 'windows':
-                name = demangle_ms(self.bv.arch, func.name)[1]
-            func_name = get_qualified_name(name)
-            return func_name
-        else:
-            return func.name
+    def get_answer_function_path(self):
+        bad_function = [func for func in self.bv.functions if re.match('_?CWE.*bad$', self.demangle_func_name(func.name))]
+        if len(bad_function) < 1:
+            raise
+        bad_function = bad_function[0]
+        self.bad_function_path.add_node(bad_function.name)
+
+        # badSink
+        bad = bad_function
+        while True:
+            prev = bad.name
+            bad = [func for func in bad.callees if re.match('.*bad.*Sink', self.demangle_func_name(func.name))]
+            if len(bad) < 1:
+                break
+            bad = bad[0]
+            self.bad_function_path.add_node(bad.name)
+            self.bad_function_path.add_edge(prev, bad.name)
+
+        # badSource
+        bad = bad_function
+        while True:
+            prev = bad.name
+            bad = [func for func in bad.callees if re.match('.*bad.*Source', self.demangle_func_name(func.name))]
+            if len(bad) < 1:
+                break
+            bad = bad[0]
+            self.bad_function_path.add_node(bad.name)
+            self.bad_function_path.add_edge(prev, bad.name)
+
+        return self.bad_function_path
