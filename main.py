44cba11d712a0a0740cef63ad5fcbb9bf5ff6953
ch4rli3kop <pch2180@gmail.com>
2023-01-11 19:38:05 +0900

    Update integer_overflow.py
    Add solution()

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ from binaryninja import *
 from utils.angr_manager import AngrManager
 import logging
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logging.basicConfig(filename='', format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 if __name__ == '__main__':
 
-    binary = 'D:\\Projects\\binary-nomaj\\C\\testcases\\CWE134_Uncontrolled_Format_String\\s01\\CWE134_Uncontrolled_Format_String__char_connect_socket_printf_45.out'
+    #binary = 'D:\\Projects\\binary-nomaj\\C\\testcases\\CWE134_Uncontrolled_Format_String\\s01\\CWE134_Uncontrolled_Format_String__char_connect_socket_printf_45.out'
     binary = 'D:\\Projects\\binary-nomaj\\C\\testcases\\CWE190_Integer_Overflow\\s01\\CWE190_Integer_Overflow__char_fscanf_multiply_67.out'
     
     bv = binaryview.BinaryViewType.get_view_of_file(binary)
     
     #from CWE134_Uncontrolled_Format_String.format_string import source_targets, sink_targets, solution
-    from CWE190_Integer_Overflow.integer_overflow import source_targets, solution
+    from CWE190_Integer_Overflow.integer_overflow import make_sources_and_sinks, solution
 
-    sources = make_targets(bv, source_targets)
-    sinks = make_arithmetic_targets(bv)
+    sources, sinks = make_sources_and_sinks(bv=bv)
 
     pf = PathFinder(bv=bv, sources=sources, sinks=sinks, option=PathGenOption.POSSIBLE_VALUE_UPDATE)
     paths = pf.generate_path()
@@ -28,7 +27,7 @@ if __name__ == '__main__':
         #path.save_graph() # if name is None, filename is random
         #path.save_bndb_file_by_path()
 
-        angr_manager = AngrManager(path=path)
-        if angr_manager.check_feasible():
-            print(f'This Path are feasible!')
-            solution(bv, path)
+        # angr_manager = AngrManager(path=path)
+        # if angr_manager.check_feasible():
+        #     print(f'This Path are feasible!')
+        solution(bv, path)
